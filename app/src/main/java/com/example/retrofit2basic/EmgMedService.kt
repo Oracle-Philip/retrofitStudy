0b5f4d87c09762a2b3a85a8aa8a038bb8763396d
philipy075 <philipy075@btsoft.kr>
2022-12-06 10:34:12 +0900

    Coroutine

--- app/src/main/java/com/example/retrofit2basic/EmgMedService.kt
+++ app/src/main/java/com/example/retrofit2basic/EmgMedService.kt
@@ -1,6 +1,7 @@
 package com.example.retrofit2basic
 
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -9,5 +10,10 @@ interface EmgMedService {
     fun getEmgMedData(@Query("KEY") KEY : String,
                       @Query("Type") Type : String): Call<EmgMedResponse>
 
+    //Coroutine으로 작동하는 서비스가 인터페이스에 추가되었다.
+    @GET("EmgMedInfo")
+    suspend fun getDataCoroutine(
+        @Query("KEY") KEY : String,
+        @Query("Type") Type : String): Response<EmgMedResponse>
 }
 
--- app/src/main/java/com/example/retrofit2basic/MainActivity.kt
+++ app/src/main/java/com/example/retrofit2basic/MainActivity.kt
@@ -6,6 +6,10 @@ import android.util.Log
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.retrofit2basic.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -40,23 +44,40 @@ class MainActivity : AppCompatActivity() {
     private fun retrofitWork(){
         val service = RetrofitApi.emgMedService
 
-        service.getEmgMedData(getString(R.string.api_key), Type = "json")
-            .enqueue(object : Callback<EmgMedResponse> {
-                override fun onResponse(
-                    call: Call<EmgMedResponse>,
-                    response: Response<EmgMedResponse>
-                ) {
-                    if(response.isSuccessful){
-                        Log.e(TAG, response.body().toString())
-//                        val result : EmgMedInfo? = response.body()?.emgMedInfo?.get(1)?.row
-                        val result : List<Row?>? = response.body()?.emgMedInfo?.get(1)?.row
-                        emgMedAdapter.submitList(result!!)
-                    }
-                }
+//        service.getEmgMedData(getString(R.string.api_key), Type = "json")
+//            //enqueue는 백그라운드 스레드에서 처리한다.
+//            //response callBack은 메인 스레드에서 처리한다.
+//            //coroutine을 사용하면 callBack을 사용하지 않아도 동일하게 잘 처리할 수 있다.
+//            .enqueue(object : Callback<EmgMedResponse> {
+//                override fun onResponse(
+//                    call: Call<EmgMedResponse>,
+//                    response: Response<EmgMedResponse>
+//                ) {
+//                    if(response.isSuccessful){
+//                        Log.e(TAG, response.body().toString())
+////                        val result : EmgMedInfo? = response.body()?.emgMedInfo?.get(1)?.row
+//                        val result : List<Row?>? = response.body()?.emgMedInfo?.get(1)?.row
+//                        emgMedAdapter.submitList(result!!)
+//                    }
+//                }
+//
+//                override fun onFailure(call: Call<EmgMedResponse>, t: Throwable) {
+//                    Log.e(TAG, t.message.toString())
+//                }
+//            })
+        CoroutineScope(Dispatchers.IO).launch {
+            val response : Response<EmgMedResponse> = service.getDataCoroutine(getString(R.string.api_key), Type = "json")
 
-                override fun onFailure(call: Call<EmgMedResponse>, t: Throwable) {
-                    Log.e(TAG, t.message.toString())
+            withContext(Dispatchers.Main){
+                if(response.isSuccessful){
+                    val result : List<Row?>? = response.body()?.emgMedInfo?.get(1)?.row
+                    result?.let {
+                        emgMedAdapter.submitList(it)
+                    }
+                } else{
+                    Log.e(TAG, response.code().toString())
                 }
-            })
+            }
+        }
     }
 }
