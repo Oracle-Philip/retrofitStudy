3abe745eb5e41e8e58a63e59bec76281a7fbeed3
philipy075 <philipy075@btsoft.kr>
2022-12-06 10:05:47 +0900

    init

---
+++ app/src/main/java/com/example/retrofit2basic/EmgMedService.kt
@@ -0,0 +1,13 @@
+package com.example.retrofit2basic
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface EmgMedService {
+    @GET("EmgMedInfo")
+    fun getEmgMedData(@Query("KEY") KEY : String,
+                      @Query("Type") Type : String): Call<EmgMedResponse>
+
+}
+
---
+++ app/src/main/java/com/example/retrofit2basic/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.retrofit2basic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.retrofit2basic.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    private val TAG : String by lazy {
+        MainActivity::class.java.simpleName
+    }
+
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val emgMedAdapter : EmgMedAdapter by lazy {
+        EmgMedAdapter()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = emgMedAdapter
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        }
+
+        binding.btnGet.setOnClickListener {
+            retrofitWork()
+        }
+    }
+
+    private fun retrofitWork(){
+        val service = RetrofitApi.emgMedService
+
+        service.getEmgMedData(getString(R.string.api_key), Type = "json")
+            .enqueue(object : Callback<EmgMedResponse> {
+                override fun onResponse(
+                    call: Call<EmgMedResponse>,
+                    response: Response<EmgMedResponse>
+                ) {
+                    if(response.isSuccessful){
+                        Log.e(TAG, response.body().toString())
+                    }
+                }
+
+                override fun onFailure(call: Call<EmgMedResponse>, t: Throwable) {
+                    Log.e(TAG, t.message.toString())
+                }
+            })
+    }
+}
